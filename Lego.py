a74026157b14740f4cb08108beeefcdb4803a729
Connor Pool <connorpool@Connors-MacBook-Pro.local>
2022-01-14 16:11:25 -0500

    saving work to date

--- Lego.py
+++ Lego.py
@@ -6,6 +6,10 @@ import util
 rebrick.init("d34210807df61e1ad97727779d78cad2")
 
 # get set info
-response = rebrick.lego.get_set(6608)
-# print(json.loads(response.read()))
+# response = rebrick.lego.get_set(6608)
+# util.print_json(json.loads(response.read()))
+
+# get part
+response = rebrick.lego.get_part(3032)
 util.print_json(json.loads(response.read()))
+
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## rebrickable API documentation
 https://github.com/Bifboy/rebrickable-API.git
+
+## rebrick Python module documentation
+https://pypi.org/project/rebrick/
+https://github.com/xxao/rebrick
